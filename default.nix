f7678f2b4c29d9b1dd33bdac6a63a34d107a8374
Jacek Generowicz <jacg@my-post-office.net>
2021-01-28 17:35:54 +0100

    Refactor default.nix

--- default.nix
+++ default.nix
@@ -2,8 +2,4 @@
   pkgs ? import nix/pkgs.nix
 }:
 
-let
-  buildIt = import nix/derivation.nix;
-in
-
-pkgs.callPackage buildIt { inherit (pkgs) boost stdenv; }
+pkgs.callPackage (import nix/derivation.nix) { inherit (pkgs) boost stdenv; }
