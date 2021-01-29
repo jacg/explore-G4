d35d7533e857474d2bbb04d7d934fd1c835bcba5
Jacek Generowicz <jacg@my-post-office.net>
2021-01-29 16:10:31 +0100

    Remove single-use particleTable variable

--- modified-examples/B1/src/B1PrimaryGeneratorAction.cc
+++ modified-examples/B1/src/B1PrimaryGeneratorAction.cc
@@ -20,9 +20,8 @@ B1PrimaryGeneratorAction::B1PrimaryGeneratorAction()
   fParticleGun  = new G4ParticleGun(n_particle);
 
   // default particle kinematic
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4ParticleDefinition* particle
-    = particleTable->FindParticle("gamma");
+    = G4ParticleTable::GetParticleTable()->FindParticle("gamma");
   fParticleGun->SetParticleDefinition(particle);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
   fParticleGun->SetParticleEnergy(6.*MeV);
