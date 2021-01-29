caf9a8d0b6ef163417497c8b3a24ba052b1d70a3
Jacek Generowicz <jacg@my-post-office.net>
2021-01-29 16:39:23 +0100

    Cosmetics

--- modified-examples/B1/include/B1EventAction.hh
+++ modified-examples/B1/include/B1EventAction.hh
@@ -6,27 +6,20 @@
 
 class B1RunAction;
 
-/// Event action class
-///
 
-class B1EventAction : public G4UserEventAction
-{
-  public:
-    B1EventAction(B1RunAction* runAction);
-    virtual ~B1EventAction();
+class B1EventAction : public G4UserEventAction {
+public:
+  B1EventAction(B1RunAction* runAction);
+  virtual ~B1EventAction() {}
 
-    virtual void BeginOfEventAction(const G4Event* event);
-    virtual void EndOfEventAction(const G4Event* event);
+  virtual void BeginOfEventAction(const G4Event* event);
+  virtual void   EndOfEventAction(const G4Event* event);
 
-    void AddEdep(G4double edep) { fEdep += edep; }
+  void AddEdep(G4double edep) { fEdep += edep; }
 
-  private:
-    B1RunAction* fRunAction;
-    G4double     fEdep;
+private:
+  B1RunAction* fRunAction;
+  G4double     fEdep;
 };
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
-
-    
