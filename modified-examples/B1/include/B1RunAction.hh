335482c4270fcaeb6138b222273fcc8017d0447c
Jacek Generowicz <jacg@my-post-office.net>
2021-01-29 17:39:27 +0100

    The usual cosmetics, in B1RunAction

--- modified-examples/B1/include/B1RunAction.hh
+++ modified-examples/B1/include/B1RunAction.hh
@@ -7,27 +7,24 @@
 
 class G4Run;
 
-/// Run action class
-///
-/// In EndOfRunAction(), it calculates the dose in the selected volume
-/// from the energy deposit accumulated via stepping and event actions.
-/// The computed dose is then printed on the screen.
+/// EndOfRunAction(), calculates the dose in the selected volume from the energy
+/// deposit accumulated via stepping and event actions. The computed dose is
+/// then printed on the screen.
 
-class B1RunAction : public G4UserRunAction
-{
-  public:
-    B1RunAction();
-    virtual ~B1RunAction();
+class B1RunAction : public G4UserRunAction {
+public:
+  B1RunAction();
+  virtual ~B1RunAction() {}
 
-    // virtual G4Run* GenerateRun();
-    virtual void BeginOfRunAction(const G4Run*);
-    virtual void   EndOfRunAction(const G4Run*);
+  // virtual G4Run* GenerateRun();
+  virtual void BeginOfRunAction(const G4Run*);
+  virtual void   EndOfRunAction(const G4Run*);
 
-    void AddEdep (G4double edep);
+  void AddEdep (G4double edep);
 
-  private:
-    G4Accumulable<G4double> fEdep;
-    G4Accumulable<G4double> fEdep2;
+private:
+  G4Accumulable<G4double> fEdep;
+  G4Accumulable<G4double> fEdep2;
 };
 
 #endif
