b2f148012a5ddc11af399d4b7252936d15cc12b8
Jacek Generowicz <jacg@my-post-office.net>
2021-02-01 14:23:57 +0100

    Comment on G4 ownership magic

--- abracadabra/src/run_action.cc
+++ abracadabra/src/run_action.cc
@@ -15,7 +15,7 @@ run_action::run_action()
 : G4UserRunAction()
 , edep(0)
 , edep2(0) {
-  // add new units for dose
+  // G4 takes ownership of these by *MAGIC*
   new G4UnitDefinition{"milligray", "milliGy" , "Dose", 1.e-3  * gray};
   new G4UnitDefinition{"microgray", "microGy" , "Dose", 1.e-6  * gray};
   new G4UnitDefinition{ "nanogray",  "nanoGy" , "Dose", 1.e-9  * gray};
