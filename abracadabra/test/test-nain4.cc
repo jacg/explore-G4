592dfddf4c2fbab4a0561968c0a776803b3c00c9
Jacek Generowicz <jacg@my-post-office.net>
2021-03-09 18:57:10 +0100

    Parametrize materials test

--- abracadabra/test/test-nain4.cc
+++ abracadabra/test/test-nain4.cc
@@ -10,10 +10,11 @@ TEST_CASE("nain4", "[nain]") {
   auto run_manager = G4RunManager::GetRunManager();
 
   SECTION("NIST materials") {
-    auto name = "G4_AIR";
-    auto nain_air = nain4::material(name);
-    auto nist_air = G4NistManager::Instance()->FindOrBuildMaterial(name);
-    REQUIRE(nain_air == nist_air);
+    auto material_name = GENERATE("G4_AIR", "G4_WATER", "G4_H", "G4_A-150_TISSUE");
+    auto nain_material = nain4::material(material_name);
+    auto nist_material = G4NistManager::Instance()->FindOrBuildMaterial(material_name);
+    REQUIRE(nain_material == nist_material);
+    REQUIRE(nain_material != nullptr);
   }
 
 }
