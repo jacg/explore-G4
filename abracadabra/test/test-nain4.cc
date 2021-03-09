37a970a145c1f4fff8eb3615a54820450c42ca9f
Jacek Generowicz <jacg@my-post-office.net>
2021-03-09 18:41:39 +0100

    Test that Nain air is the same as NIST air

--- abracadabra/test/test-nain4.cc
+++ abracadabra/test/test-nain4.cc
@@ -1,10 +1,19 @@
 #include "nain4.hh"
 
+#include <G4NistManager.hh>
+#include <G4RunManager.hh>
+
 #include <catch2/catch.hpp>
 
 TEST_CASE("nain4", "[nain]") {
 
-  REQUIRE(1*2 == 2);
-  REQUIRE(3*2 == 6);
+  auto run_manager = G4RunManager::GetRunManager();
+
+  SECTION("NIST materials") {
+    auto name = "G4_AIR";
+    auto nain_air = nain4::material(name);
+    auto nist_air = G4NistManager::Instance()->FindOrBuildMaterial(name);
+    REQUIRE(nain_air == nist_air);
+  }
 
 }
