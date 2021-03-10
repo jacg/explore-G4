fe298cfa3d25849fb8141e499a33c3ba3f036db2
Jacek Generowicz <jacg@my-post-office.net>
2021-03-10 12:20:18 +0100

    Adjust section names

--- abracadabra/test/test-nain4.cc
+++ abracadabra/test/test-nain4.cc
@@ -22,7 +22,7 @@ TEST_CASE("nain4", "[nain]") {
 
   auto run_manager = G4RunManager::GetRunManager();
 
-  SECTION("NIST materials") {
+  SECTION("material NIST") {
     auto material_name = GENERATE("G4_AIR", "G4_WATER", "G4_H", "G4_A-150_TISSUE");
     auto nain_material = nain4::material(material_name);
     auto nist_material = G4NistManager::Instance()->FindOrBuildMaterial(material_name);
@@ -42,21 +42,21 @@ TEST_CASE("nain4", "[nain]") {
     }
   }
 
-  SECTION("logical volumes") {
-      auto water = nain4::material("G4_WATER");
-      auto lx = 1 * m;
-      auto ly = 2 * m;
-      auto lz = 3 * m;
-      auto box = nain4::volume<G4Box>("test_box", water, lx, ly, lz);
-      auto density = water->GetDensity();
-      CHECK(box->TotalVolumeEntities() == 1);
-      CHECK(box->GetMass() / kg        == Approx(8 * lx * ly * lz * density / kg));
-      CHECK(box->GetMaterial()         == water);
-      CHECK(box->GetName()             == "test_box");
+  SECTION("volume") {
+    auto water = nain4::material("G4_WATER");
+    auto lx = 1 * m;
+    auto ly = 2 * m;
+    auto lz = 3 * m;
+    auto box = nain4::volume<G4Box>("test_box", water, lx, ly, lz);
+    auto density = water->GetDensity();
+    CHECK(box->TotalVolumeEntities() == 1);
+    CHECK(box->GetMass() / kg        == Approx(8 * lx * ly * lz * density / kg));
+    CHECK(box->GetMaterial()         == water);
+    CHECK(box->GetName()             == "test_box");
 
-      auto solid = box->GetSolid();
-      CHECK(solid->GetCubicVolume() / m3 == Approx(8 *  lx    * ly    * lz     / m3));
-      CHECK(solid->GetSurfaceArea() / m2 == Approx(8 * (lx*ly + ly*lz + lz*lx) / m2));
-      CHECK(solid->GetName()             == "test_box");
+    auto solid = box->GetSolid();
+    CHECK(solid->GetCubicVolume() / m3 == Approx(8 *  lx    * ly    * lz     / m3));
+    CHECK(solid->GetSurfaceArea() / m2 == Approx(8 * (lx*ly + ly*lz + lz*lx) / m2));
+    CHECK(solid->GetName()             == "test_box");
   }
 }
