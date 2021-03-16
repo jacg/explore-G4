208b60f30c12a20aa206c825c9daa3437ed036be
Jacek Generowicz <jacg@my-post-office.net>
2021-03-16 09:43:32 +0100

    Fix visualization on non-NixOS by addding nixGL

--- shell.nix
+++ shell.nix
@@ -4,6 +4,17 @@
 
 let
   derivation = pkgs.callPackage (import ./nix/derivation.nix) {};
+
+# ----- Conditional inclusion ----------------------------------------------------
+  nothing = pkgs.coreutils;
+  linux      = drvn: if pkgs.stdenv.isLinux  then drvn else nothing;
+
+  # ----- Getting OpenGL to work on non-NixOS --------------------------------------
+  nixGL = pkgs.callPackage "${builtins.fetchTarball {
+    url = https://github.com/guibou/nixGL/archive/7d6bc1b21316bab6cf4a6520c2639a11c25a220e.tar.gz;
+    #sha256 = pkgs.lib.fakeSha256;
+    sha256 = "02y38zmdplk7a9ihsxvnrzhhv7324mmf5g8hmxqizaid5k5ydpr3";
+  }}/nixGL.nix" {};
 in
 
 pkgs.llvmPackages_11.stdenv.mkDerivation {
@@ -20,6 +31,9 @@ pkgs.llvmPackages_11.stdenv.mkDerivation {
     pkgs.catch2
     pkgs.cmake-language-server
     pkgs.gdb
+    # For graphics hardware matching on non-NixOS
+    pkgs.libGL
+    (linux nixGL.nixGLDefault)
   ];
 
 }
