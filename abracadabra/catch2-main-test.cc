fe5b66f430cd7dd20474bba53c3dc62359efc7ec
Jacek Generowicz <jacg@my-post-office.net>
2021-03-23 14:24:13 +0100

    Ensure that G4RunManager is single-threaded

--- abracadabra/catch2-main-test.cc
+++ abracadabra/catch2-main-test.cc
@@ -44,7 +44,7 @@ int main(int argc, char** argv) {
 
   // Construct the default run manager
   auto run_manager = unique_ptr<G4RunManager>
-    {G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default)};
+    {G4RunManagerFactory::CreateRunManager(G4RunManagerType::SerialOnly)};
 
   // Set mandatory initialization classes
 
