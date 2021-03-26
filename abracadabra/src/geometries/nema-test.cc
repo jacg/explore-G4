c1f6a82a8e2c93917660932579186610e7ce1449
Jacek Generowicz <jacg@my-post-office.net>
2021-03-26 19:16:20 +0100

    Remove superfluous intermediate variable

--- abracadabra/src/geometries/nema-test.cc
+++ abracadabra/src/geometries/nema-test.cc
@@ -12,16 +12,15 @@ TEST_CASE("NEMA phantom geometry", "[nema][geometry]") {
 
   using std::setw;
 
-  auto phantom = build_nema_phantom{}
+  auto geometry = build_nema_phantom{}
     .sphere(10*mm, 2.8)
     .sphere(13*mm, 2.8)
     .sphere(17*mm, 2.8)
     .sphere(22*mm, 2.8)
     .sphere(28*mm, 0)
     .sphere(37*mm, 0)
-    .build();
-
-  auto geometry = phantom.geometry();
+    .build()
+    .geometry();
 
   std::cout << std::endl;
   for (auto v: geometry) {
