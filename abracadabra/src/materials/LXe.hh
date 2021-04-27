0bb20b37f5b0f84ee7efb82320b35aa069f97457
Jacek Generowicz <jacg@my-post-office.net>
2021-04-27 17:54:59 +0200

    Move LXe properties out to own files

---
+++ abracadabra/src/materials/LXe.hh
@@ -0,0 +1,5 @@
+#include <G4MaterialPropertiesTable.hh>
+
+G4double LXe_refractive_index(G4double energy);
+G4MaterialPropertiesTable* LXe_optical_material_properties();
+G4double LXe_Scintillation(G4double energy);
