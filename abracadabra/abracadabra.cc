4dca9ea02ad53e828d980d33348dfd6a2b27613d
Jacek Generowicz <jacg@my-post-office.net>
2021-04-29 14:25:43 +0200

    Add lots of exploratory code to abracadabra.cc

--- abracadabra/abracadabra.cc
+++ abracadabra/abracadabra.cc
@@ -41,25 +41,82 @@ int main(int argc, char** argv) {
   // For use with phantom_in_cylinder
   auto phantom = a_nema_phantom();
 
-  n4::sensitive_detector::process_hits_fn make_noise = [](G4Step* step) {
-    auto track    = step->GetTrack();
-    auto particle = track->GetParticleDefinition();
-    auto name     = particle->GetParticleName();
-    std::cout << "LOOK AT MEEEEEEEEEEEE! " << name << std::endl;
+  int last_interesting_event = 0;
+  size_t count_interesting_event = 0;
+  size_t count_511s              = 0;
+  size_t count_gammas            = 0;
+  size_t events_with_electrons = 0;  bool got_electron = false;
+  size_t events_with_optphots  = 0;  bool got_optphot  = false;
+  // clang-format off
+  n4::sensitive_detector::process_hits_fn make_noise = [&last_interesting_event, &count_interesting_event,
+                                                        &events_with_electrons, &got_electron,
+                                                        &events_with_optphots , &got_optphot,
+                                                        &count_511s, &count_gammas](G4Step* step) {
+    auto pos      = step -> GetPostStepPoint() -> GetPosition();
+    auto track    = step -> GetTrack();
+    auto energy   = track -> GetKineticEnergy();
+    auto particle = track -> GetParticleDefinition();
+    auto name     = particle -> GetParticleName();
+    auto id       = track -> GetTrackID();
+    auto event_id = n4::event_number();
+    auto tg = track -> GetGlobalTime();
+
+    auto vol_pre_name = step -> GetPreStepPoint()  -> GetTouchable() -> GetVolume() -> GetName();
+    auto vol_postname = step -> GetPostStepPoint() -> GetTouchable() -> GetVolume() -> GetName();
+
+    // proc_name = step->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessName();
+
+    if (event_id > last_interesting_event) { count_interesting_event++; }
+    last_interesting_event = event_id;
+
+    if (name == "gamma") {
+      count_gammas++;
+      if (energy > 510.99 * keV && energy < 511.01 * keV) {
+        count_511s++;
+      }
+    }
+
+    if (name == "e-") {
+      if (!got_electron) { events_with_electrons++; }
+      got_electron = true;
+    }
+
+    if (name == "opticalphoton") {
+      if (!got_optphot) { events_with_optphots++; }
+      got_electron = true;
+    }
+    std::cout << std::setw (4) << events_with_electrons << " /"
+              << std::setw (4) << events_with_optphots << " /"
+              << std::setw (4) << round(100 - (100.0 * count_511s / count_gammas) ) << "% /"
+              << std::setw (4) << count_interesting_event << " /"
+              << std::setw (4) << event_id << ' '
+              << std::setw(15) << name << ' '
+              << std::setw (4) << id << ' '
+              << std::setw (4) << round(energy / keV) << " keV " << pos << ' '
+              << std::setw(10) << tg << ' '
+
+      //<< vol_pre_name << ' ' << vol_postname
+              << std::endl;
     return true; // Still don't know what this means!
   };
+
+  n4::sensitive_detector::end_of_event_fn eoe = [&last_interesting_event, &got_electron](auto) {
+    if (n4::event_number() == last_interesting_event) {std::cout << std::endl;}
+    got_electron = false;
+  };
+
   // pick one:
-  auto sd = new n4::sensitive_detector{"Noisy_detector", make_noise, {}};
+  auto sd = new n4::sensitive_detector{"Noisy_detector", make_noise, eoe};
   //auto sd = nullptr;
 
   // Set mandatory initialization classes
 
   // run_manager takes ownership of geometry
   run_manager -> SetUserInitialization(new n4::geometry{[&phantom, sd]() -> G4VPhysicalVolume* {
-    // Pick one ...
-    return phantom_in_cylinder(phantom, 70*mm, sd);
+    // Pick one (ensure that generator (below) is compatible) ...
+    return phantom_in_cylinder(phantom, 200*mm, 40*mm, sd);
+    return cylinder_lined_with_hamamatsus(150*mm, 70*mm, 50*mm, sd);
     return phantom.geometry();
-    return cylinder_lined_with_hamamatsus(70*mm, 70*mm, sd);
     return imas_demonstrator(nullptr);
     return square_array_of_sipms(sd);
     return nain4::place(sipm_hamamatsu_blue(true, sd)).now();
@@ -77,8 +134,18 @@ int main(int argc, char** argv) {
   run_manager->SetUserInitialization(new n4::actions{
       new n4::generator{[&phantom](G4Event* event) {
         // Pick one that matches geometry
-        //generate_back_to_back_511_keV_gammas(event, {}, 0);
+        // generate_back_to_back_511_keV_gammas(event, {}, 0);
         phantom.generate_primaries(event);
+
+        // auto optphot = nain4::find_particle("geantino");
+        // auto p = G4ThreeVector{0,1,0} * 7 * eV;
+        // double time = 0;
+        // auto vertex =      new G4PrimaryVertex({0, 65*mm, 3*mm}, time);
+        // auto primary = new G4PrimaryParticle(optphot,  p.x(),  p.y(),  p.z());
+        // primary -> SetPolarization({1,0,0});
+        // vertex->SetPrimary(primary);
+        // event -> AddPrimaryVertex(vertex);
+
       }}});
 
   // Initialize visualization
@@ -100,17 +167,20 @@ int main(int argc, char** argv) {
     // interactive mode
     {
       ui_manager -> ApplyCommand("/control/execute init_vis.mac");
-      ui_manager -> ApplyCommand("/run/beamOn 100");
+      ui_manager -> ApplyCommand("/PhysicsList/RegisterPhysics G4EmStandardPhysics_option4");
+      ui_manager -> ApplyCommand("/PhysicsList/RegisterPhysics G4OpticalPhysics");
+      ui_manager -> ApplyCommand("/vis/scene/endOfEventAction accumulate 100");
+      //ui_manager -> ApplyCommand("/run/beamOn 1");
       //nain4::silence _{G4cout};
-      int PHI = 150; int THETA = 160;
+      int PHI = 150; int THETA = 160; int THETAF = 175;
       auto view = [&ui_manager](auto theta, auto phi) {
         ui_manager->ApplyCommand("/vis/viewer/set/viewpointThetaPhi "
                                  + std::to_string(theta) + ' ' + std::to_string(phi));
       };
       {
         nain4::silence _{G4cout};
-        for (int phi  =PHI  ; phi  <360+PHI  ; ++phi  ) { view(THETA, phi); }
-        for (int theta=THETA; theta<360+THETA; ++theta) { view(theta, PHI); }
+        for (int phi  =PHI  ; phi  <360+PHI   ; phi  +=4) { view(THETA, phi); }
+        for (int theta=THETA; theta<360+THETAF; theta+=4) { view(theta, PHI); }
       }
       ui -> SessionStart();
     }
