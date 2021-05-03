a3c054cd746543f5d7207305ad1325f900d3177b
Jacek Generowicz <jacg@my-post-office.net>
2021-05-03 09:14:51 +0200

    Fix bug in interpolate

--- abracadabra/src/utils/interpolate.hh
+++ abracadabra/src/utils/interpolate.hh
@@ -8,10 +8,13 @@ template<class F> auto map(F f, std::vector<double> const& xs) {
   return ys;
 }
 
+// TODO needs testing
 template<class F> auto interpolate(F f, size_t N, double min, double max) {
   std::vector<double> xs(N);
   std::vector<double> ys(N);
-  iota(begin(xs), end(xs), (max - min) / N);
+  auto step = (max - min) / N;
+  size_t n = 0;
+  generate (begin(xs), end(xs), [min, step, &n](){ return  min + (n++ * step); });
   transform(begin(xs), end(xs), begin(ys), f);
   return make_tuple(xs, ys);
 }
