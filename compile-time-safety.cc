2f8f0c3e59c855273da9aaee722a324e2828e3d5
Jacek Generowicz <jacg@my-post-office.net>
2021-05-10 16:53:38 +0200

    Alternative layout of compile-time-safety proof of concept

--- compile-time-safety.cc
+++ compile-time-safety.cc
@@ -7,34 +7,34 @@ class ready {
   int b;
 public:
   void use() { std::cout << f << ' ' << b << std::endl; }
-  friend class needs_bar;
-  friend class needs_foo;
-};
 
-// ----- helpers: not fully-configured states ----------------------------------
-struct needs_foo {
-  needs_foo(int b) : b{b} {}
-  ready foo(int f) { return {f, b}; }
+  // ----- incompletely-specified states ---------------------------------------
 private:
-  int b;
+  struct needs_foo {
+    needs_foo(int b) : b{b} {}
+    ready foo(int f) { return {f, b}; }
+  private:
+    int b;
+  };
+
+  struct needs_bar {
+    needs_bar(int f) : f{f} {}
+    ready bar(int b) { return {f, b}; }
+  private:
+    int f;
+  };
+  // ----- entry point to the construction interface ----------------------------
+public:
+  struct make {
+    needs_bar foo(int f) { return {f}; }
+    needs_foo bar(int b) { return {b}; }
+  };
 };
 
-struct needs_bar {
-  needs_bar(int f) : f{f} {}
-  ready bar(int b) { return {f, b}; }
-private:
-  int f;
-};
-
-// ----- interface entry point --------------------------------------------------
-struct needs_foo_and_bar {
-  needs_bar foo(int f) { return {f}; }
-  needs_foo bar(int b) { return {b}; }
-};
 
 // ----- Client code that must specify foo and bar exactly once each ------------
 int main() {
-  needs_foo_and_bar()
+  ready::make()
     .foo(1)
     .bar(2)
     .use();
