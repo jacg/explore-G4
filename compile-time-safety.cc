822c947a5e02eee24c81f67ec52c6bb6494d3598
Jacek Generowicz <jacg@my-post-office.net>
2021-05-11 10:17:17 +0200

    Add explicit use cases in compile-time-safety sample

--- compile-time-safety.cc
+++ compile-time-safety.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// ----- The class we want to use ----------------------------------------------
+// ----- A class with two mandatory configurables ------------------------------
 class ready {
   ready(int f, int b) : f{f}, b{b} {}
   int f;
@@ -34,8 +34,36 @@ public:
 
 // ----- Client code that must specify foo and bar exactly once each ------------
 int main() {
-  ready::make()
-    .foo(1)
-    .bar(2)
-    .use();
+  auto not_set = ready::make{};
+
+  auto foo_set = not_set.foo(1);
+  auto bar_set = not_set.bar(2);
+
+  // compile-time errors:
+  // foo_set.foo(666) ; // setting foo more than once
+  // bar_set.bar(666) ; // setting bar more than once
+  // not_set.use();     // neither foo nor bar set before use
+  // foo_set.use();     // bar not set before use
+  // bar_set.use();     // foo not set before use
+
+  auto all_set_a = foo_set.bar(3); // foo already set, now setting bar
+  auto all_set_b = bar_set.foo(4); // bar already set, now setting foo
+
+  all_set_a.use(); // Can use because both foo and bar have been set
+  all_set_b.use(); // Same again, though setting done in reverse order
+
+  // ----- Single line versions ------------------------------
+
+  // Compile-time errors
+
+  // ready::make().use();   // foo and bar missing
+  // ready::make().foo(1).use();  // bar missing
+  // ready::make().bar(2).use();  // foo missing
+  // ready::make().foo(1).foo(2); // foo conflict
+  // ready::make().bar(1).bar(2); // bar conflict
+
+  // Successes
+
+  ready::make().foo(1).bar(2).use();
+  ready::make().bar(1).foo(2).use();
 }
