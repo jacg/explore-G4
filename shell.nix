6a43e44d41afd9c6e55c6d1c1fded64e91fcd914
Jacek Generowicz <jacg@my-post-office.net>
2021-05-28 08:19:12 +0200

    Switch back to clang 11 to recover missing clangd

--- shell.nix
+++ shell.nix
@@ -17,15 +17,15 @@ let
   }}/nixGL.nix" {};
 in
 
-pkgs.llvmPackages_12.stdenv.mkDerivation {
+pkgs.llvmPackages_11.stdenv.mkDerivation {
   inherit (derivation) name;
   nativeBuildInputs = derivation.nativeBuildInputs ++ [
-    pkgs.clang_12
+    pkgs.clang_11
   ];
 
   buildInputs = derivation.buildInputs ++ [
 #    pkgs.clang-tools
-    pkgs.clang_12
+    pkgs.clang_11
     pkgs.cmake
     pkgs.catch2
     pkgs.cmake-language-server
