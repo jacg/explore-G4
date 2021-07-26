f74ac197aba9ee78717676c9ba02b208ad9958b2
Jacek Generowicz <jacg@my-post-office.net>
2021-07-26 22:42:56 +0200

    Rename active -> window

--- abracadabra/src/geometries/sipm.cc
+++ abracadabra/src/geometries/sipm.cc
@@ -15,26 +15,26 @@ using nain4::vis_attributes;
 
 
 G4LogicalVolume* sipm::build() {
-  auto act_half_x = half.x() - act.margin_x;
-  auto act_half_y = half.y() - act.margin_y;
-  auto act_half_z = act.dz / 2;
+  auto act_half_x = half.x() - win.margin_x;
+  auto act_half_y = half.y() - win.margin_y;
+  auto act_half_z = win.dz / 2;
   auto pre_z = act_half_z / 1000;
-  auto prename = "pre_" + act.name;
+  auto prename = "pre_" + win.name;
   auto premat  = pre_active_material_;
   auto vol_body = volume<G4Box>(    name,     mat,     half.x(),     half.y(),     half.z());
-  auto vol_act  = volume<G4Box>(act.name, act.mat, act_half_x  , act_half_y  , act_half_z);
+  auto vol_act  = volume<G4Box>(win.name, win.mat, act_half_x  , act_half_y  , act_half_z);
   auto vol_pre  = volume<G4Box>( prename,  premat, act_half_x  , act_half_y  ,      pre_z);
 
   vol_pre -> SetSensitiveDetector(sensitive_detector);
 
   // ----- visibility -------------------------------------------------------------
   vol_body -> SetVisAttributes(    vis_attributes);
-  vol_act  -> SetVisAttributes(act.vis_attributes);
-  vol_pre  -> SetVisAttributes(act.vis_attributes);
+  vol_act  -> SetVisAttributes(win.vis_attributes);
+  vol_pre  -> SetVisAttributes(win.vis_attributes);
 
   // ----- geometrical relationship between components ----------------------------
-  auto z_act_in_body = act.dz/2 - half.z();
-  auto z_pre_in_body =  pre_z/2 - half.z() + act.dz;
+  auto z_act_in_body = win.dz/2 - half.z();
+  auto z_pre_in_body =  pre_z/2 - half.z() + win.dz;
   place(vol_pre).in(vol_body).at(0,0,z_pre_in_body).name("fake_active").now();
   place(vol_act).in(vol_body).at(0,0,z_act_in_body).name("true_active").now();
   return vol_body;
@@ -109,7 +109,7 @@ G4LogicalVolume* sipm_hamamatsu_blue(G4bool visible, G4VSensitiveDetector* sd) {
   auto vis_body = visible ?    col::Yellow()                  : va().visible(false);
   auto vis_act  = visible ? va(col::Blue()).force_solid(true) : va().visible(false);
 
-  auto active = sipm_window("Quartz_window")
+  auto window = sipm_window("Quartz_window")
     .thickness(0.1*mm)
     .material(fr4)
     .vis(vis_act);
@@ -117,7 +117,7 @@ G4LogicalVolume* sipm_hamamatsu_blue(G4bool visible, G4VSensitiveDetector* sd) {
   return sipm("Hamamatsu_Blue", sd)
     .material("G4_Si")
     .size(6*mm, 6*mm, 0.6*mm)
-    .active(active)
+    .window(window)
     .fake_active_material(fr4)
     .vis(vis_body)
     .build();
