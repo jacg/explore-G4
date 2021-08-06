44cbb723f73c9ccec129803c0af1beca7315d28f
Jacek Generowicz <jacg@my-post-office.net>
2021-08-07 01:04:58 +0200

    Move combine_geometries from abracadabra to nain4

--- abracadabra/abracadabra.cc
+++ abracadabra/abracadabra.cc
@@ -207,33 +207,6 @@ struct phantom_t {
   const n4::geometry::construct_fn geometry;
 };
 
-auto combine_geometries(G4VPhysicalVolume* phantom, G4VPhysicalVolume* detector) {
-  auto detector_envelope = detector -> GetLogicalVolume();
-  auto phantom_envelope  =  phantom -> GetLogicalVolume();
-  auto phantom_contents = phantom_envelope -> GetDaughter(0) -> GetLogicalVolume(); // TODO Can we avoid this?
-
-  // Check whether phantom envelope fits inside detector envelope, with margin.
-  auto& pbox = dynamic_cast<G4Box&>(* phantom_envelope -> GetSolid());
-  auto& dbox = dynamic_cast<G4Box&>(*detector_envelope -> GetSolid());
-  auto expand = false;
-  auto make_space = [&expand](auto p, auto d) {
-    auto space_needed = std::max(p * 1.1, d);
-    if (space_needed > d) { expand = true; }
-    return space_needed;
-  };
-  auto x = make_space(pbox.GetXHalfLength(), dbox.GetXHalfLength());
-  auto y = make_space(pbox.GetYHalfLength(), dbox.GetYHalfLength());
-  auto z = make_space(pbox.GetZHalfLength(), dbox.GetZHalfLength());
-  // Expand detector envelope if needed
-  if (expand) {
-    auto new_box = new G4Box(detector_envelope->GetName(), x, y, z);
-    detector_envelope -> SetSolid(new_box);
-  }
-
-  n4::place(phantom_contents).in(detector_envelope).now();
-  return detector;
-};
-
 // ============================== MAIN =======================================================
 int main(int argc, char** argv) {
 
--- abracadabra/nain4/nain4.cc
+++ abracadabra/nain4/nain4.cc
@@ -2,6 +2,7 @@
 
 #include <G4EmStandardPhysics_option4.hh>
 #include <G4OpticalPhysics.hh>
+#include <G4Box.hh>
 #include <FTFP_BERT.hh>
 #include <G4PVPlacement.hh>
 #include <G4String.hh>
@@ -111,6 +112,36 @@ void use_our_optical_physics(G4RunManager* run_manager, G4int verbosity) {
     physics_list -> RegisterPhysics(new G4OpticalPhysics{});
     run_manager  -> SetUserInitialization(physics_list);
 } // run_manager owns physics_list
+
+
+
+G4VPhysicalVolume* combine_geometries(G4VPhysicalVolume* phantom, G4VPhysicalVolume* detector) {
+  auto detector_envelope = detector -> GetLogicalVolume();
+  auto phantom_envelope  =  phantom -> GetLogicalVolume();
+  auto phantom_contents = phantom_envelope -> GetDaughter(0) -> GetLogicalVolume(); // TODO Can we avoid this?
+
+  // Check whether phantom envelope fits inside detector envelope, with margin.
+  auto& pbox = dynamic_cast<G4Box&>(* phantom_envelope -> GetSolid());
+  auto& dbox = dynamic_cast<G4Box&>(*detector_envelope -> GetSolid());
+  auto expand = false;
+  auto make_space = [&expand](auto p, auto d) {
+    auto space_needed = std::max(p * 1.1, d);
+    if (space_needed > d) { expand = true; }
+    return space_needed;
+  };
+  auto x = make_space(pbox.GetXHalfLength(), dbox.GetXHalfLength());
+  auto y = make_space(pbox.GetYHalfLength(), dbox.GetYHalfLength());
+  auto z = make_space(pbox.GetZHalfLength(), dbox.GetZHalfLength());
+  // Expand detector envelope if needed
+  if (expand) {
+    auto new_box = new G4Box(detector_envelope->GetName(), x, y, z);
+    detector_envelope -> SetSolid(new_box);
+  }
+
+  n4::place(phantom_contents).in(detector_envelope).now();
+  return detector;
+};
+
 
 } // namespace nain4
 
