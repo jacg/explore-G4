76e1fbc9037e05ee3d4a73c6be0525ee3d3790eb
Jacek Generowicz <jacg@my-post-office.net>
2021-09-16 17:59:51 +0200

    Const-qualify returned materials and volumes

--- abracadabra/src/geometries/inspect.cc
+++ abracadabra/src/geometries/inspect.cc
@@ -14,11 +14,11 @@ world_geometry_inspector::world_geometry_inspector(G4RunManager* run_manager)
 }
 
 
-G4Material* world_geometry_inspector::material_at(const G4ThreeVector& point) {
+G4Material const * world_geometry_inspector::material_at(const G4ThreeVector& point) {
   return volume_at(point) -> GetLogicalVolume() -> GetMaterial();
 }
 
-G4VPhysicalVolume* world_geometry_inspector::volume_at(const G4ThreeVector& point) {
+G4VPhysicalVolume const* world_geometry_inspector::volume_at(const G4ThreeVector& point) {
   auto touchable = std::make_unique<G4TouchableHistory>();
   navigator -> LocateGlobalPointAndUpdateTouchable(point, touchable.get(), false);
   return touchable -> GetVolume();
