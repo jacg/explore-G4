6b3f7441ba6c6ae22d885d9af4babefcfff08398
Jacek Generowicz <jacg@my-post-office.net>
2023-06-27 11:22:20 +0200

    Turn shell.nix into a flake.nix wrapper

--- shell.nix
+++ shell.nix
@@ -1,54 +1,15 @@
-{
-  pkgs ? import nix/pkgs.nix
-}:
+# Adapts flake.nix to support legacy `nix-shell`
 
-let
-  derivation = pkgs.callPackage (import ./nix/derivation.nix) {};
-
-# ----- Conditional inclusion ----------------------------------------------------
-  nothing = pkgs.coreutils;
-  linux      = drvn: if pkgs.stdenv.isLinux  then drvn else nothing;
-
-  # ----- Getting OpenGL to work on non-NixOS --------------------------------------
-  nixGL = pkgs.callPackage "${builtins.fetchTarball {
-    url = https://github.com/guibou/nixGL/archive/7d6bc1b21316bab6cf4a6520c2639a11c25a220e.tar.gz;
-    #sha256 = pkgs.lib.fakeSha256;
-    sha256 = "02y38zmdplk7a9ihsxvnrzhhv7324mmf5g8hmxqizaid5k5ydpr3";
-  }}/nixGL.nix" {};
-in
-
-pkgs.llvmPackages_13.stdenv.mkDerivation {
-  inherit (derivation) name;
-  nativeBuildInputs = derivation.nativeBuildInputs ++ [
-    pkgs.clang_13
-    (pkgs.python39.withPackages (ps: [ps.docopt]))
-  ];
-
-  buildInputs = derivation.buildInputs ++ [
-#    pkgs.clang-tools
-    pkgs.clang_13
-    pkgs.cmake
-    pkgs.catch2
-    pkgs.cmake-language-server
-    pkgs.gdb
-    pkgs.hdf5
-    pkgs.highfive
-    pkgs.hdfview
-    pkgs.just
-    # For big-endian writing of density image in raw format
-    pkgs.poco
-    pkgs.pcre # Needed by Poco
-    # For graphics hardware matching on non-NixOS
-    pkgs.libGL
-    (linux nixGL.nixGLDefault)
-    # Syntax colouring in gdb
-    pkgs.python3Packages.pygments
-    # profiling
-    (linux pkgs.linuxPackages.perf)
-    (linux pkgs.oprofile)
-    (linux pkgs.kcachegrind)
-    (linux pkgs.graphviz) # used by kcachegrind
-    (linux pkgs.flamegraph)
-  ];
-
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
